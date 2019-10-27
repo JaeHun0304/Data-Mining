49c7044880a5f31f3c43f82c245deadf076e1238
JaeHun Jung <45073917+JaeHun0304@users.noreply.github.com>
2019-10-26 23:15:17 -0500

    modified:   Assignment4-2.py 	modified:   Assignment4-3.py

--- Assignment4-2.py
+++ Assignment4-2.py
@@ -1,66 +1,24 @@
+from sklearn.cluster import DBSCAN
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Initialize data points given in Figure 15.12
 samples = np.array([[5,8], [10,8], [11,8], [6,7], [10,7], [12,7], [13,7], [5,6], [10,6], [13,6], [6,5], [9,4], [11,5], [14,6], [15,5], 
 		   [2,4], [3,4], [5,4], [6,4], [7,4], [15,4], [3,3], [7,3], [8,2]])
 
-cluster_id = [None] * len(samples)
-cores = []
-cores_index = []
-noise = []
+# Define DBSCAN clustering with min distance 2 and min points 3 with default euclidean distance method
+clustering = DBSCAN(eps=2, min_samples=3).fit(samples)
+# print clustering parameters, core samples, and clustered labels
+print(clustering)
+print(clustering.core_sample_indices_)
+print(clustering.labels_)
 
-def density_connected(x, k):
-	for i in range(len(samples)):
-		if np.linalg.norm(samples[x] - samples[i]) <= 2:
-			if cluster_id[i] == None:
-				cluster_id[i] = k
-			if i in cores_index and cluster_id[i] == None:
-				density_connected(i, k)
-
-
-
+# Filter the samples based on the cluster id and perform scatter plot
 for i in range(len(samples)):
-	counter = 0
-	for j in range(len(samples)):
-		if np.linalg.norm(samples[i]-samples[j]) <= 2 and i != j:
-			counter = counter + 1
-	if counter >= 3:
-		cores.append(samples[i])
-		cores_index.append(i)
-
-print(cores)
-
-if np.linalg.norm(samples[3] - samples[0]) <= 2:
-	print("a is directly reachable from d")
-else:
-	print("a is not directly reachable from d")
-
-for k in range(len(cores_index)):
-	if cluster_id[cores_index[k]] == None:
-		cluster_id[cores_index[k]] = k
-		density_connected(cores_index[k], k)
-
-print(cluster_id)
-
-for i in range(len(cluster_id)):
-	if cluster_id[i] == None:
-		print("Sample " + str(i) + " is noise point")
-
-for i in range(len(samples)):
-	if i == 0 or i == 3 or i == 7 or i == 10:
+	if clustering.labels_[i] == 0:
 		plt.scatter(samples[i][0], samples[i][1], color="yellow")
-	if i == 1 or i == 2 or i == 4 or i == 8:
+	elif clustering.labels_[i] == 1:
 		plt.scatter(samples[i][0], samples[i][1], color="green")
-	if i == 5 or i == 6 or i == 9:
-		plt.scatter(samples[i][0], samples[i][1], color="blue")
-	if i == 13 or i == 14:
-		plt.scatter(samples[i][0], samples[i][1], color="red")
-	if i == 15 or i == 16 or i ==17 or i == 21:
-		plt.scatter(samples[i][0], samples[i][1], color="brown")
-	if i == 18 or i == 19 or i == 22:
-		plt.scatter(samples[i][0], samples[i][1], color="black")
-	if i == 11 or i == 12 or i == 20 or i == 23:
-		plt.scatter(samples[i][0], samples[i][1], color="purple")
 
 plt.title('Scatter plot of density clustered data points')
 plt.show()
--- Assignment4-3.py
+++ Assignment4-3.py
@@ -2,11 +2,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# define x-axis with linespace from 0 to 18.0 with 5000 samples
+# and y with 2D list that each has 5000 zero element array
 x = np.linspace(0.0, 18.0, num=5000)
 y = np.array([np.zeros(5000)] * 5)
 
+# define samples
 samples = [1, 5, 6, 9, 15]
 
+# if the windows falls into 3/2 = 1.5, accumulate 1/15(1/nh), otherwise accumulate zero
 for i in range(len(samples)):
 	for j in range(len(x)):
 		if abs(x[j] - samples[i]) <= 1.5:
@@ -14,6 +18,7 @@ for i in range(len(samples)):
 		else:
 			y[i][j] += 0
 
+# print all piecewise constant functions
 plt.plot(x, y[0])
 plt.show()
 plt.plot(x, y[1])
@@ -23,4 +28,9 @@ plt.show()
 plt.plot(x, y[3])
 plt.show()
 plt.plot(x, y[4])
+plt.show()
+
+# print total convoluted function
+y_total = y[0] + y[1] + y[2] + y[3] + y[4]
+plt.plot(x, y_total)
 plt.show()
